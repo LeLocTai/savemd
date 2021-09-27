ba10ece7fd9cbf7a955abe911295191baebe39eb
Tai Le <contact@leloctai.com>
2021-09-27 14:38:01 +0700

    some signing settings

--- scripts/webextsign.js
+++ scripts/webextsign.js
@@ -2,8 +2,9 @@
 const { exec } = require('child_process');
 require('dotenv').config()
 
-exec(`web-ext sign ---source-dir dist/webext-prod`, (err, stdout, stderr) =>
+exec(`web-ext sign --source-dir dist/webext-prod --channel=unlisted`, (err, stdout, stderr) =>
 {
+    console.log(err);
     console.log(stdout);
     console.error(stderr);
 })
